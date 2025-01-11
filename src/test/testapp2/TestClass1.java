a2cdb803a023edbef2c9dff2c4b75c271e0ce4f8
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2025-01-11 15:49:12 +0400

    test

--- src/test/testapp2/TestClass1.java
+++ src/test/testapp2/TestClass1.java
@@ -2,6 +2,6 @@ package test.testapp2;
 
 public class TestClass1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Rəna");
     }
 }
